95fd53c77607f8d4d1247f8e16ac4242de909348
mhazzard <miramardesign@gmail.com>
2022-04-21 15:54:44 -0300

    fix tests

--- src/app/components/calendar/calendar.component.spec.ts
+++ src/app/components/calendar/calendar.component.spec.ts
@@ -2,6 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CalendarComponent } from './calendar.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { ReminderFormInlineModule } from '../reminder-form-inline/reminder-form-inline.module';
+import { ReminderFormModule } from '../reminder-form/reminder-form.module';
+import { WeatherIconModule } from '../weather-icon/weather-icon.module';
+import { CalendarRoutingModule } from './calendar-routing.module';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
@@ -9,8 +16,19 @@ describe('CalendarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-
+      imports: [
+        HttpClientTestingModule,
+   
+        CommonModule,
+        FormsModule,
+    
+        CalendarRoutingModule,
+        SharedModule,
+        ReminderFormInlineModule,
+        ReminderFormModule,
+        WeatherIconModule,
+      ],
+         
       declarations: [ CalendarComponent ]
     })
     .compileComponents();
--- src/app/components/reminder-form-inline/reminder-form-inline.component.spec.ts
+++ src/app/components/reminder-form-inline/reminder-form-inline.component.spec.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { CalendarViewDateType } from 'src/app/interfaces/calender-models';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { WeatherIconModule } from '../weather-icon/weather-icon.module';
 
 import { ReminderFormInlineComponent } from './reminder-form-inline.component';
 
-describe('ReminderFormComponent', () => {
+describe('ReminderFormInlineComponent', () => {
   let component: ReminderFormInlineComponent;
   let fixture: ComponentFixture<ReminderFormInlineComponent>;
 
@@ -27,8 +28,33 @@ describe('ReminderFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReminderFormInlineComponent);
     component = fixture.componentInstance;
+
+    component.formModeData = {
+      dayToUpdate: -1,
+      mode: 'create',
+      evtI: 0,
+    };
+
+    component.formData = {
+      desc: '1234567890  1234567890  1234567890  1234567890  1234567890  ',
+      // desc: '1234567890  ',
+      city: 'Roario',
+      time: '9:00 a',
+      weather: 'slight chance of apacolypse',
+    };
+
+    component.calendarLayoutDay = {
+      day: 20,
+      year: 2022,
+      month: 3,
+      dateType: CalendarViewDateType.Current,
+      events: [],
+      selected: true,
+    };
+
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -52,5 +78,6 @@ describe('ReminderFormComponent', () => {
 
     expect(component.isValidForm(component.formData)).toBeFalse();
   });
+
 
 });
